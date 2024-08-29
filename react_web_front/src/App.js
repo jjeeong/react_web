4c315e320a7628d210f4b3ca53785d37e1db5bd9
문정원 <moonjw25@naver.com>
2024-08-29 17:59:35 +0900

    header, footer, 기본 화면 작성

--- react_web_front/src/App.js
+++ react_web_front/src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
+import Main from "./components/common/Main";
 
 function App() {
   return (
     <div className="wrap">
       <Header />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
