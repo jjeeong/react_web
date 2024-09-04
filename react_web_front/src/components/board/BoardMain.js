7b6096a702888d50d22c4a5a94111b8d980de759
문정원 <moonjw25@naver.com>
2024-09-04 12:03:39 +0900

    페이징처리, 글 작성 폼

--- react_web_front/src/components/board/BoardMain.js
+++ react_web_front/src/components/board/BoardMain.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import BoardList from "./BoardList";
 import "./board.css";
+import BoardWrite from "./BoardWrite";
 
 const BoardMain = () => {
   return (
     <Routes>
       <Route path="list" element={<BoardList />} />
+      <Route path="write" element={<BoardWrite />} />
     </Routes>
   );
 };
